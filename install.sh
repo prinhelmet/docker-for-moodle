aa0f9e0b895531a298f8d664c82b45d8eea82bcf
J.A. Jiménez <66381987+prinhelmet@users.noreply.github.com>
2022-03-02 17:52:33 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# install.sh
+
+curl -sSL https://raw.githubusercontent.com/prinhelmet/docker-for-moodle/master/docker-compose.yml > docker-compose.yml
+curl -sSL https://raw.githubusercontent.com/prinhelmet/docker-for-moodle/master/custom-php.ini > custom-php.ini
+
+mkdir private_html
+mkdir private_html/moodledata
+mkdir public_html
+mkdir public_html/plugins
+cd public_html
+git clone git://git.moodle.org/moodle.git
+git branch --track MOODLE_311_STABLE origin/MOODLE_311_STABLE
+git checkout MOODLE_311_STABLE
+cd ..
+chgrp -R www-data public_html
+chgrp -R www-data private_html
+chmod -R g+w private_html
