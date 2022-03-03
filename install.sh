590ff7722de3557dbccb600636d9fc6326ea52f6
J.A. Jiménez <66381987+prinhelmet@users.noreply.github.com>
2022-03-03 08:40:16 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,10 +4,13 @@
 curl -sSL https://raw.githubusercontent.com/prinhelmet/docker-for-moodle/master/docker-compose.yml > docker-compose.yml
 curl -sSL https://raw.githubusercontent.com/prinhelmet/docker-for-moodle/master/custom-php.ini > custom-php.ini
 
+# Folder structure
 mkdir private_html
 mkdir private_html/moodledata
 mkdir public_html
 mkdir public_html/plugins
+
+# Moodle Clone from git (or create public_html/moodle)
 cd public_html
 git clone git://git.moodle.org/moodle.git
 cd moodle
@@ -15,6 +18,8 @@ git branch --track MOODLE_311_STABLE origin/MOODLE_311_STABLE
 git checkout MOODLE_311_STABLE
 cd ..
 cd ..
+
+# Change groups & Privileges
 chgrp -R www-data public_html
 chgrp -R www-data private_html
 chmod -R g+w private_html
