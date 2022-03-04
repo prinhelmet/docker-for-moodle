ec402e53a243d586847b414bba3b4ec6d4c0b47b
Juan Andres Jimenez <j.c.jimenez@accenture.com>
2022-03-04 11:58:46 +0100

    Change moodle to html folder for symbolic links

--- install.sh
+++ install.sh
@@ -10,14 +10,17 @@ mkdir public_html/plugins
 # Create installation & configuration files
 curl -sSL https://raw.githubusercontent.com/prinhelmet/docker-for-moodle/master/docker-compose.yml > docker-compose.yml
 curl -sSL https://raw.githubusercontent.com/prinhelmet/docker-for-moodle/master/custom-php.ini > custom-php.ini
-curl -sSL https://raw.githubusercontent.com/prinhelmet/docker-for-moodle/main/public_html/plugins/config.php > public_html/plugins/config.php
 
 # Moodle Clone from git (or create public_html/moodle)
 cd public_html
-git clone git://git.moodle.org/moodle.git
-cd moodle
+git clone git://git.moodle.org/moodle.git html
+cd html
 git branch --track MOODLE_311_STABLE origin/MOODLE_311_STABLE
 git checkout MOODLE_311_STABLE
+curl -sSL https://raw.githubusercontent.com/prinhelmet/docker-for-moodle/main/config.php > config.php
+cd ..
+cd plugins
+ln -s ../html/config.php
 cd ..
 cd ..
 
