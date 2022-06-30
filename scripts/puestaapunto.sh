26d203b9e9418c2bf297eea3df55d4a887fec0b9
Juan Andres Jimenez <j.c.jimenez@accenture.com>
2022-06-30 13:31:22 +0200

    Update scripts and enviroment

--- scripts/puestaapunto.sh
+++ scripts/puestaapunto.sh
@@ -7,8 +7,11 @@ fi
 
 echo "Instalando moodle"
 docker exec $1 /bin/bash -c "php /var/www/html/moodle/admin/cli/install_database.php --lang=es --adminuser=admin --adminpass=moodle --adminemail=admin@nomail.com --fullname=Moodle --shortname=moodle --agree-license"
+echo "Haciendo ejecutables los scripts disponibles"
 docker exec $1 /bin/bash -c "chmod +x /var/www/html/scripts/*.sh"
+echo "Instalando composer (de php)"
 docker exec $1 /bin/bash -c "/var/www/html/scripts/instalar_composer.sh"
+echo "Instalando moosh"
 docker exec $1 /bin/bash -c "/var/www/html/scripts/instalar_moosh.sh"
-echo "Cambiando grupo de www-data"
-docker exec $1 /bin/bash -c "chown -R www-data:www-data /var/www"
+echo "Cambiando propietario y grupo las carpetas de moodle y moodledata a www-data"
+docker exec $1 /bin/bash -c "chown -R www-data:www-data /var/www/html/moodle*"
